fb3ebabdd3ad36158c1c3d68427e0c6c306d2747
DPHoran <dhoran@ryerson.ca>
2016-10-30 22:08:36 -0400

    Added business categories, filtered licenses by cancellation date.

--- bizlicdb_clean.R
+++ bizlicdb_clean.R
@@ -9,4 +9,13 @@ licenses$Cancel.Date1 <- as.Date(format(strptime(licenses$Cancel.Date, "%d-%b-%Y
 
 ## Subset on businesses that deal with food
 levels(licenses$Category)
-sub_lic <- subset(licenses, Category %in% c("BOULEVARD CAFE","EATING ESTABLISHMENT","ENTERTAINMENT ESTABLISHMENT/NIGHTCLUB","MOBILE VENDING (FOOD TRUCK)","MOBILE VENDING (ICE CREAM TRUCK)","RETAIL STORE (FOOD)","THEATRE"))
+sub_lic <- subset(licenses, Category %in% c("ADULT ENTERTAINMENT CLUB","BILLIARD HALL","BOULEVARD CAFE","BOWLING HOUSE","CURBLANE VENDING","EATING ESTABLISHMENT","ENTERTAINMENT ESTABLISHMENT/NIGHTCLUB","MOBILE VENDING (FOOD TRUCK)","MOBILE VENDING (ICE CREAM TRUCK)","MOTORIZED REFRESHMENT VEHICLE OWNER","NON-MOTORIZED REFRESHMENT VEHICLE OWNER","PLACE OF AMUSEMENT","PUBLIC HALL","RETAIL STORE (FOOD)","THEATRE"))
+
+## Remove business licenses cancelled prior to Aug 31, 2014.
+sub_lic <- subset(sub_lic, (is.na(Cancel.Date1) == T | Cancel.Date1 > "2014/08/30"))
+
+## Create a new attribute for Forward Sortation Area (first three digits of Postal Code)
+sub_lic$FSA <- substr(sub_lic$Licence.Address.Line.3,1,3)
+length(unique(sub_lic$FSA))
+
+
